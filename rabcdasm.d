c09f91465982624b36bb0f7419a55d2f66d0eb23
ZS-NVB <73255861+ZS-NVB@users.noreply.github.com>
2020-10-21 21:19:51 +0100

    Update rabcdasm.d

--- rabcdasm.d
+++ rabcdasm.d
@@ -15,24 +15,51 @@
  *  You should have received a copy of the GNU General Public License
  *  along with RABCDAsm.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+ //modified to also include abcexport and to only output a single asasm file
 
 module rabcdasm;
 
 import std.file;
 import std.path;
+import std.conv;
+import std.stdio;
+import std.format;
+import swffile;
 import abcfile;
 import asprogram;
 import disassembler;
 
-void main(string[] args)
-{
-	if (args.length == 1)
-		throw new Exception("No arguments specified");
-	foreach (arg; args[1..$])
-	{
-		scope abc = ABCFile.read(cast(ubyte[])read(arg));
-		scope as = ASProgram.fromABC(abc);
-		scope disassembler = new Disassembler(as, stripExtension(arg), stripExtension(baseName(arg)));
-		disassembler.disassemble();
+void main(string[] args) {
+	if (args.length == 1) throw new Exception("No swf specified");
+	string swfName = args[1];
+	string asasmName = (2 < args.length ? args[2] : stripExtension(args[1]) ~ ".asasm");
+	ubyte[] abcBytes;
+	try {
+		scope swf = SWFFile.read(cast(ubyte[])read(swfName));
+		uint count = 0;
+		foreach (ref tag; swf.tags) if ((tag.type == TagType.DoABC || tag.type == TagType.DoABC2)) {
+			if (++count == 2) break;
+			else if (tag.type == TagType.DoABC) abcBytes = tag.data;
+			else {
+				auto p = tag.data.ptr+4; // skip flags
+				while (*p++) {} // skip name
+				abcBytes = tag.data[p-tag.data.ptr..$];
+			}
+		}
+		if (count == 0) throw new Exception("No DoABC tags found");
+		else if (1 < count) throw new Exception("More than 1 DoABC tag found");
+	} catch (Exception e) throw new Exception("Error while processing %s: %s".format(swfName, e));
+	auto abc = ABCFile.read(abcBytes);
+	auto as = ASProgram.fromABC(abc);
+	auto disassembler = new Disassembler(as);
+	string[string] strings = disassembler.disassemble();
+	ubyte[] output;
+	foreach (filename, data; strings) {
+		output ~= cast(ubyte[])filename;
+		output ~= '\0';
+		output ~= cast(ubyte[])data;
+		output ~= '\0';
 	}
+	std.file.write(asasmName, output);
 }
